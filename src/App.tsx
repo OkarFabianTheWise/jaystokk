eefa6f2038e4a19c86f6c7fd6f94ccc4a7a48333
orkarfabianthewise <orkarmelch@gmail.com>
2024-10-05 17:19:30 +0100

    Unused Import fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home'; // Import the new Home component
 import Add from './components/Add'; // Import your Add Product page component
 import Register from './components/Register'; // Import your Register page component
